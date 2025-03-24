8f824e784d56e6d3a6b67b3ca59c7c189c8ba515
Gabriel Mors <gabrielmors210@gmail.com>
2025-03-24 23:53:10 +0000

    criei novo projeto Xcode

---
+++ Exemplo-Git/Exemplo-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exemplo-Git
+//
+//  Created by Gabriel Mors on 24/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Gabriel Mors")
+    }
+
+
+}
+

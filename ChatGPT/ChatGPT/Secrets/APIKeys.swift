7d19ce07828189e2a80e0b372de63ad1a5b12b20
Gabriel Mors <gabrielmors210@gmail.com>
2025-06-12 19:53:06 -0400

    removendo minha key

--- ChatGPT/ChatGPT/Secrets/APIKeys.swift
+++ ChatGPT/ChatGPT/Secrets/APIKeys.swift
@@ -6,5 +6,5 @@
 //
 
 struct APIKeys {
-    static let authToken = "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"
+    static let authToken = ""
 }

5309422f8a4c0cddb5348706ae7c021e3e8dbe3c
Gabriel Mors Pulga <gabriel.mors.pulga@avanade.com>
2025-07-02 18:48:20 -0400

    Finalizando app de estudo sobre Singleton

--- Singleton/Singleton/main.swift
+++ Singleton/Singleton/main.swift
@@ -24,7 +24,7 @@ final class Logger { // Impede que a classe seja herdada.
     }
 }
 
-Logger.shared.log(message: "Meu primeiro Singleton)")
+Logger.shared.log(message: "Meu primeiro Singleton")
 // Outro singleton
 Logger.shared.log(message: "Meu segundo Singleton")
 
@@ -57,4 +57,58 @@ class DetalheDoProduto {
 var microondas = DetalheDoProduto()
 print(microondas.nomeUsuario) // Gabriel
 
+
+// MARK: - Class ou Struct em Singleton?
+
+
+final class SingletonClass {
+    
+    static let shared = SingletonClass() // Instância única e compartilhada
+    
+    private init() { } // Inicializador privado para impedir a criação de outras instâncias
+    
+    var state: Int = 10
+}
+
+
+func testClassSingleton() {
+    print("Valor da classe antes da atualização é: \(SingletonClass.shared.state)")// 10
+    
+    SingletonClass.shared.state = 50
+    
+    print("Valor da classe antes da atualização é: \(SingletonClass.shared.state)")// 50
+    
+    let newObjt = SingletonClass.shared // Isso não cria uma nova instância. newObjt é apenas outra referência para a mesma instância do singleton.
+    
+    newObjt.state = 20
+    
+    print("Valor da classe após a atualização é: \(SingletonClass.shared.state)")// 20
+    print("Valor do objt após a atualização é: \(newObjt.state)")// 20
+    
+}
+
+testClassSingleton()
+
+
+struct StructSingleton {
+    static var shared = StructSingleton()
+    
+    private init() { }
+    
+    var state: Int = 10
+}
+
+
+func testStructSingleton() {
+    print("Valor da Struct antes da atualização é: \(SingletonClass.shared.state)")// 10
+    
+    StructSingleton.shared.state = 50
+    var newObj = StructSingleton.shared // Faz uma cópia
+    newObj.state = 20
+    
+    print("Valor da Struct após a atualização é: \(StructSingleton.shared.state)")//50
+    print("Valor do objt após a atualização é: \(newObj.state)")// 20
+}
+
+testStructSingleton()
 
